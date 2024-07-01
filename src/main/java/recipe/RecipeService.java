420337e0a348e0a22bb0936e6062502b1de9d558
Hale <itshalenurgurpinar@gmail.com>
2024-07-01 15:27:43 +0300

    .

--- src/main/java/recipe/RecipeService.java
+++ src/main/java/recipe/RecipeService.java
@@ -4,13 +4,12 @@ import lombok.RequiredArgsConstructor;
 
 public class RecipeService {
 
-    rivate final RecipeRepository recipeRepository;
+    private final RecipeRepository recipeRepository;
     @RequiredArgsConstructor
     public RecipeResponse findById(Long id) {
         return recipeRepository.findById(id)
                 .map(recipe -> new RecipeResponse(
-                        recipe,
-                        recipe.getRecipeIngredients().stream().map(RecipeIngredientResponse::new).toList()))
+                        recipe)
                 .orElseThrow(() -> {
                     log.error(NOT_FOUND_RECIPE);
                     return new NoSuchElementFoundException(NOT_FOUND_RECIPE);
